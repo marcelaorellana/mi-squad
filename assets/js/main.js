3d6ba35cc933acd76a2bb3b4b2d32f728e56c119
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-04 22:57:33 -0300

    escribe los hobbies, color de fondo y linea separadora

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,9 @@ var arr =[
 	new miembroSquad("Gaby Bravo", "28 años", ["pintar", "ver anime", "jugar Aion"]),
 	new miembroSquad("Karina Laroze", "21 años", ["bailar"]),
 	new miembroSquad("Mily Rivas", "18 años", ["leer", "ver películas"]),
-	new miembroSquad("Marcela Orellana", "31 años", ["Andar en Bicicleta", "ver series coreanas"])
+	new miembroSquad("Marcela Orellana", "31 años", ["Andar en Bicicleta", "ver series coreanas"]),
+	new miembroSquad("Angela Aliaga", "32 años", ["leer", "escuchar música", "jugar donkey kong country"]),
+	new miembroSquad("Dani Riesco", "35 años", ["ver películas"])
 ];
 
 var miDiv = document.getElementById("miembras");
@@ -25,13 +27,32 @@ function mostrarSquad(value){
 	var parrafo2 = document.createElement("p");
 	var textAux2 = document.createTextNode("Edad: " + value.edad );
 
+	var parrafo4 = document.createElement("p");
+	var textAux4 = document.createTextNode("Hobbies:");
+
+	var linea = document.createElement("hr");
+
 	parrafo1.appendChild(textAux1); 
 	divAux.appendChild(parrafo1); 
 
     parrafo2.appendChild(textAux2); 
     divAux.appendChild(parrafo2);
 
+    parrafo4.appendChild(textAux4); 
+    divAux.appendChild(parrafo4);
+
+    divAux.appendChild(linea);
+
     miDiv.appendChild(divAux);
 
+ 	value.hobbies.forEach(function(val){
+ 		var ul = document.createElement("ul");
+		var parrafo3 = document.createElement("li");
+		var textAux3 = document.createTextNode(val + "  ");
 
+		parrafo3.appendChild(textAux3);
+		ul.appendChild(parrafo3);
+		divAux.appendChild(ul);  
+		divAux.appendChild(linea);
+	}); 
 }
