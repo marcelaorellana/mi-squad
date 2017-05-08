625ba590d089a7999222928e56f2012a5e382d2d
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-07 21:14:02 -0300

    se agregó foto de perfil, contador aún no cuenta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,25 @@
-function miembroSquad(id, nombre, edad, hobbies){
+function miembroSquad(id, foto, nombre, edad, hobbies){
 	this.id = id;
+	this.foto = foto;
 	this.nombre = nombre;
 	this.edad = edad;
 	this.hobbies = hobbies;
 }
 
 var arr =[
-	new miembroSquad(1,"Gaby Bravo", "28 años", ["pintar", "ver anime", "jugar Aion"]),
-	new miembroSquad(2,"Karina Laroze", "21 años", ["bailar"]),
-	new miembroSquad(3,"Mily Rivas", "18 años", ["leer", "ver películas"]),
-	new miembroSquad(4,"Marcela Orellana", "31 años", ["Andar en Bicicleta", "ver series coreanas"]),
-	new miembroSquad(5,"Angela Aliaga", "32 años", ["leer", "escuchar música", "jugar donkey kong country"]),
-	new miembroSquad(6,"Dani Riesco", "35 años", ["ver películas"])
+	new miembroSquad(1,"eevee.png","Gaby Bravo", "28 años", ["pintar", "ver anime", "jugar Aion"]),
+	new miembroSquad(2,"growlithe.png","Karina Laroze", "21 años", ["bailar"]),
+	new miembroSquad(3,"oddish.png","Mily Rivas", "18 años", ["leer", "ver películas"]),
+	new miembroSquad(4,"snorlax.png","Marcela Orellana", "31 años", ["Andar en Bicicleta", "ver series coreanas"]),
+	new miembroSquad(5,"psyduck.png","Angela Aliaga", "32 años", ["leer", "escuchar música", "jugar donkey kong country"]),
+	new miembroSquad(6,"horsea.png","Dani Riesco", "35 años", ["ver películas"])
 ];
 
 var miDiv = document.getElementById("miembras");
 
 var divAux, textAux1, textAux2, textAux3;
-
+var cont = 0;
+var textAux6;
 arr.forEach(mostrarSquad);
 
 function mostrarSquad(value){
@@ -31,6 +33,13 @@ function mostrarSquad(value){
 	var parrafo4 = document.createElement("p");
 	var textAux4 = document.createTextNode("Hobbies:");
 
+	var oImg = document.createElement("img");
+		oImg.setAttribute('src', value.foto);
+		oImg.setAttribute('alt', 'na');
+		oImg.setAttribute('height', '100px');
+		oImg.setAttribute('width', '100px');
+		
+
 	var comentario = document.createElement("textarea");
 	comentario.name = 'post';
 	comentario.id = value.id;
@@ -38,7 +47,7 @@ function mostrarSquad(value){
 	var parrafo5 = document.createElement("p");
 		parrafo5.id = "opinion";
 	var parrafo6 = document.createElement("p");
-	var cont = 0;
+	
 
 	var link = document.createElement("a");
 
@@ -47,6 +56,8 @@ function mostrarSquad(value){
 	//var coment = [];
 
 	divAux.appendChild(linea);
+
+	divAux.appendChild(oImg);
 
 	parrafo1.appendChild(textAux1); 
 	divAux.appendChild(parrafo1); 
@@ -78,18 +89,15 @@ function mostrarSquad(value){
     	var br = document.createElement("br");
     
     	parrafo5.innerHTML += coment + " <a href='#' onclick='showLikes("+ comentario.id +")'>" + " Me Gusta" +"</a>" + "<br>";
-    	var textAux6 = document.createTextNode(cont);
+    	textAux6 = document.createTextNode(cont);
     	
     		/*var textAux5 = document.createTextNode(coment + "   ");
     		var linkText = document.createTextNode("Me Gusta   ");
 	    	link.title = "my title text";
 			link.href = "#";
 			var br = document.createElement("br");
-
 	    	link.appendChild(linkText);
-			
 	    	parrafo5.appendChild(textAux5);
-
 	    	link.appendChild(br);
 	    	parrafo5.appendChild(link);
 	    	parrafo5.appendChild(br);*/
@@ -104,8 +112,13 @@ function mostrarSquad(value){
 
     miDiv.appendChild(divAux);
 
-    function showLikes(){
-    	cont ++;
-    }
+
 
 }
+function showLikes(contId){
+		
+    	cont ++;
+    	
+    	textAux6.innerHTML = cont;
+    	console.log(cont);
+    }
